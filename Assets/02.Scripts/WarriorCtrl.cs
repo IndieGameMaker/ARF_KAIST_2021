f143b140566e86135cc487d87d1ce7be1a30961b
Lee JaeHyun <myleje@gmail.com>
2021-02-02 11:34:08 +0900

    .

--- Assets/02.Scripts/WarriorCtrl.cs
+++ Assets/02.Scripts/WarriorCtrl.cs
@@ -22,6 +22,12 @@ public class WarriorCtrl : MonoBehaviour
 
     void Update()
     {
-        
+        if (isMove == false) return;
+
+        //목적지를 바라보는 벡터 계산 (목적지좌표 - 현재좌표)
+        Vector3 dir = movePos - transform.position;
+        //회전각도를 계산
+        //회전처리 , 보간함수(선형보간 Lerp, 스피어리컬보간 Slerp)
+        //직진이동
     }
 }
