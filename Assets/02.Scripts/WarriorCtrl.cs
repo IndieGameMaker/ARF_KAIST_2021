a0b57ad6f7a064b08b5198584dd2e5cc1c0ed340
Lee JaeHyun <myleje@gmail.com>
2021-02-02 11:28:56 +0900

    .

---
+++ Assets/02.Scripts/WarriorCtrl.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarriorCtrl : MonoBehaviour
+{
+    public float moveSpeed = 0.2f;
+    public float turnSpeed = 20.0f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
