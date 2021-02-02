827731a775004af8c2dab3ecb8afa4c77ca78bf1
Lee JaeHyun <myleje@gmail.com>
2021-02-02 11:31:00 +0900

    .

--- Assets/02.Scripts/WarriorCtrl.cs
+++ Assets/02.Scripts/WarriorCtrl.cs
@@ -6,13 +6,20 @@ public class WarriorCtrl : MonoBehaviour
 {
     public float moveSpeed = 0.2f;
     public float turnSpeed = 20.0f;
+    public Vector3 movePos = Vector3.zero;
+    public bool isMove = false;
 
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    public void MoveWarrior(Vector3 pos)
+    {
+        movePos = pos;
+        isMove = true;
+    }
+
     void Update()
     {
         
