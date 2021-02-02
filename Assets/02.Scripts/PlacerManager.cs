35efb657fb41791f8d052096392c52ba3c2a45bd
Lee JaeHyun <myleje@gmail.com>
2021-02-02 11:10:22 +0900

    .

--- Assets/02.Scripts/PlacerManager.cs
+++ Assets/02.Scripts/PlacerManager.cs
@@ -1,3 +1,4 @@
+#pragma warning disable IDE0051, IDE0052, IDE0044
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,11 +10,12 @@ public class PlacerManager : MonoBehaviour
     public GameObject warrior;
     private bool isCreated = false;
 
+    private ARRaycastManager raycastManager;
     private List<ARRaycastHit> hits = new List<ARRaycastHit>();
-    
+
     void Start()
     {
-        
+        raycastManager = GetComponent<ARRaycastManager>();
     }
 
     // Update is called once per frame
